3ba10c87855c15a92fd67165cfcab7e0e847bfc0
hengabay7 <hengabay7@gmail.com>
2023-04-24 13:04:16 +0300

    2442023

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
+import action from './action.js';
+
+
+const app =express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+const port = process.env.PORT;
+
+app.use('/api',action);
+
+mongoose.connect(process.env.MOBGO_URL)
+.then(results => {
+    app.listen(port, ()=> {
+        console.log(`Server is running via port ${port}`);
+    })
+})
+.catch(error => {
+    console.log(error.message);
+})
+
